d61b19c17575ab6ef9867fb6d0911f8e0a1d2105
Ju Reyms <65686072+JuReyms@users.noreply.github.com>
2022-04-05 17:02:24 +0200

    Create montana-cans-black.js

---
+++ montana-cans-black.js
@@ -0,0 +1,909 @@
+export default [
+    {
+        "hex": "#F4E593",
+        "name": "SMASH137´S POTATO",
+        "ref": "BLK 1005"
+    },
+    {
+        "hex": "#FFE668",
+        "name": "EASTER YELLOW",
+        "ref": "BLK 1010"
+    },
+    {
+        "hex": "#FFDA15",
+        "name": "KICKING YELLOW",
+        "ref": "BLK 1025"
+    },
+    {
+        "hex": "#FCC000",
+        "name": "YELLOW",
+        "ref": "BLK 1030"
+    },
+    {
+        "hex": "#F59E01",
+        "name": "MELON YELLOW",
+        "ref": "BLK 1045"
+    },
+    {
+        "hex": "#D37E0A",
+        "name": "TOPAZ",
+        "ref": "BLK 1050"
+    },
+    {
+        "hex": "#A34C2E",
+        "name": "HAZLE",
+        "ref": "BLK 1060"
+    },
+    {
+        "hex": "#6F2F23",
+        "name": "PECAN NUT",
+        "ref": "BLK 1070"
+    },
+    {
+        "hex": "#612316",
+        "name": "MAROON",
+        "ref": "BLK 1080"
+    },
+    {
+        "hex": "#E0A600",
+        "name": "INDIAN SPICE",
+        "ref": "BLK 1110"
+    },
+    {
+        "hex": "#B69822",
+        "name": "MASALA",
+        "ref": "BLK 1120"
+    },
+    {
+        "hex": "#A18114",
+        "name": "DELHI",
+        "ref": "BLK 1130"
+    },
+    {
+        "hex": "#6F592A",
+        "name": "BOMBAY",
+        "ref": "BLK 1140"
+    },
+    {
+        "hex": "#F49F24",
+        "name": "JUICE",
+        "ref": "BLK 2060"
+    },
+    {
+        "hex": "#EF800D",
+        "name": "CLOCKWORK ORANGE",
+        "ref": "BLK 2070"
+    },
+    {
+        "hex": "#EA640C",
+        "name": "PURE ORANGE",
+        "ref": "BLK 2075"
+    },
+    {
+        "hex": "#E85011",
+        "name": "HALLOWEEN",
+        "ref": "BLK 2085"
+    },
+    {
+        "hex": "#E3001B",
+        "name": "CODE RED",
+        "ref": "BLK 2093"
+    },
+    {
+        "hex": "#F08C48",
+        "name": "TOMORROW",
+        "ref": "BLK 2110"
+    },
+    {
+        "hex": "#EB6A27",
+        "name": "ATOM´S MEGABLAST",
+        "ref": "BLK 2120"
+    },
+    {
+        "hex": "#C4091C",
+        "name": "FIRE ROSE",
+        "ref": "BLK 3020"
+    },
+    {
+        "hex": "#6D0827",
+        "name": "CARDINAL",
+        "ref": "BLK 3062"
+    },
+    {
+        "hex": "#3F2224",
+        "name": "MERLOT",
+        "ref": "BLK 3065"
+    },
+    {
+        "hex": "#331028",
+        "name": "CHERRY",
+        "ref": "BLK 3070"
+    },
+    {
+        "hex": "#510029",
+        "name": "WINEGUM",
+        "ref": "BLK 3080"
+    },
+    {
+        "hex": "#F5B8C5",
+        "name": "MISS PIGGY",
+        "ref": "BLK 3100"
+    },
+    {
+        "hex": "#EF8EB0",
+        "name": "PINK CADILLAC",
+        "ref": "BLK 3120"
+    },
+    {
+        "hex": "#E7478A",
+        "name": "PINK PANTHER",
+        "ref": "BLK 3130"
+    },
+    
+    {
+        "hex": "#C1005E",
+        "name": "PUNK PINK",
+        "ref": "BLK 3145"
+    },
+    {
+        "hex": "#E94190",
+        "name": "BEAST",
+        "ref": "BLK 3148"
+    },
+    {
+        "hex": "#AA0073",
+        "name": "FREAK",
+        "ref": "BLK 3150"
+    },
+    {
+        "hex": "#981569",
+        "name": "PURPLE RAIN",
+        "ref": "BLK 3155"
+    },
+    {
+        "hex": "#AD0F6C",
+        "name": "GOOD TIMES",
+        "ref": "BLK 3160"
+    },
+    {
+        "hex": "#7E154E",
+        "name": "IMPERATOR",
+        "ref": "BLK 3170"
+    },
+    {
+        "hex": "#F7C0B7",
+        "name": "COCKTAIL",
+        "ref": "BLK 3200"
+    },
+    {
+        "hex": "#F29A84",
+        "name": "MR. CRAB",
+        "ref": "BLK 3220"
+    },
+    {
+        "hex": "#EE8363",
+        "name": "SUSHI",
+        "ref": "BLK 3240"
+    },
+    {
+        "hex": "#F3A7B1",
+        "name": "PATPONG",
+        "ref": "BLK 3305"
+    },
+    {
+        "hex": "#EB727B",
+        "name": "PINK LEMONADE",
+        "ref": "BLK 3310"
+    },
+    {
+        "hex": "#E74963",
+        "name": "LOLLIPOP",
+        "ref": "BLK 3320"
+    },
+    {
+        "hex": "#BA0F3E",
+        "name": "BLOODY MARY",
+        "ref": "BLK 3330"
+    },
+    {
+        "hex": "#A8438F",
+        "name": "ILLUSION",
+        "ref": "BLK 3920"
+    },
+    {
+        "hex": "#9D197E",
+        "name": "MAGIC",
+        "ref": "BLK 3940"
+    },
+    {
+        "hex": "#C59FC6",
+        "name": "BUBBLE BATH",
+        "ref": "BLK 3995"
+    },
+    {
+        "hex": "#B68FBC",
+        "name": "MS. JACKSON",
+        "ref": "BLK 4000"
+    },
+    {
+        "hex": "#8C4A91",
+        "name": "MONSTER",
+        "ref": "BLK 4020"
+    },
+    {
+        "hex": "#6D2C7E",
+        "name": "PIMP VIOLET",
+        "ref": "BLK 4040"
+    },
+    {
+        "hex": "#511B57",
+        "name": "GALAXY",
+        "ref": "BLK 4060"
+    },
+    {
+        "hex": "#B1A0CA",
+        "name": "LAVENDER",
+        "ref": "BLK 4115"
+    },
+    {
+        "hex": "#A98DBD",
+        "name": "BLUE VELVET",
+        "ref": "BLK 4130"
+    },
+    {
+        "hex": "#62579B",
+        "name": "ROYAL PURPLE",
+        "ref": "BLK 4155"
+    },
+    {
+        "hex": "#554189",
+        "name": "WIZARD",
+        "ref": "BLK 4160"
+    },
+    {
+        "hex": "#0C0940",
+        "name": "UNIVERSE",
+        "ref": "BLK 4182"
+    },
+    {
+        "hex": "#AA959C",
+        "name": "GUT",
+        "ref": "BLK 4210"
+    },
+    {
+        "hex": "#836976",
+        "name": "KIDNEY",
+        "ref": "BLK 4230"
+    },
+    {
+        "hex": "#664B5C",
+        "name": "LIVER",
+        "ref": "BLK 4250"
+    },
+    {
+        "hex": "#DBB4C6",
+        "name": "DUMMY",
+        "ref": "BLK 4260"
+    },
+    {
+        "hex": "#986D81",
+        "name": "PLUM",
+        "ref": "BLK 4280"
+    },
+    {
+        "hex": "#5B2C40",
+        "name": "AMETHYST",
+        "ref": "BLK 4290"
+    },
+    {
+        "hex": "#CDD2DB",
+        "name": "EDELGARD",
+        "ref": "BLK 4310"
+    },
+    {
+        "hex": "#A2B0CE",
+        "name": "BRUNHILDE",
+        "ref": "BLK 4320"
+    },
+    {
+        "hex": "#6F89BC",
+        "name": "WALTRAUT",
+        "ref": "BLK 4330"
+    },
+    {
+        "hex": "#4D599A",
+        "name": "IRMGARD",
+        "ref": "BLK 4340"
+    },
+    {
+        "hex": "#6BC8E7",
+        "name": "BABY BLUE",
+        "ref": "BLK 5020"
+    },
+    {
+        "hex": "#00A2D3",
+        "name": "LIGHT BLUE",
+        "ref": "BLK 5030"
+    },
+    {
+        "hex": "#0070B8",
+        "name": "HORIZON",
+        "ref": "BLK 5070"
+    },
+    {
+        "hex": "#054B90",
+        "name": "ROYALE BLUE",
+        "ref": "BLK 5077"
+    },
+    {
+        "hex": "#00326D",
+        "name": "ULTRAMARINE",
+        "ref": "BLK 5080"
+    },
+    {
+        "hex": "#00173A",
+        "name": "DARK INDIGO",
+        "ref": "BLK 5092"
+    },
+    {
+        "hex": "#AEC2C0",
+        "name": "DOVE",
+        "ref": "BLK 5125"
+    },
+    {
+        "hex": "#52676C",
+        "name": "SEAL",
+        "ref": "BLK 5140"
+    },
+    {
+        "hex": "#364958",
+        "name": "SPACE",
+        "ref": "BLK 5160"
+    },
+    {
+        "hex": "#98B0C6",
+        "name": "TROUT",
+        "ref": "BLK 5180"
+    },
+    {
+        "hex": "#577492",
+        "name": "MARLIN",
+        "ref": "BLK 5185"
+    },
+    {
+        "hex": "#264B68",
+        "name": "WHALE",
+        "ref": "BLK 5190"
+    },
+    {
+        "hex": "#00A0B0",
+        "name": "ICE BLUE",
+        "ref": "BLK 5200"
+    },
+    {
+        "hex": "#97C4E1",
+        "name": "LENOR",
+        "ref": "BLK 5210"
+    },
+    {
+        "hex": "#5F97C6",
+        "name": "BLUE LAGOON",
+        "ref": "BLK 5230"
+    },
+    {
+        "hex": "#00579E",
+        "name": "KNOCK OUT BLUE",
+        "ref": "BLK 5250"
+    },
+    {
+        "hex": "#24327D",
+        "name": "SORRENTO BLUE",
+        "ref": "BLK 5270"
+    },
+    {
+        "hex": "#E3DE2A",
+        "name": "PISTACHIO",
+        "ref": "BLK 6000"
+    },
+    {
+        "hex": "#C9D215",
+        "name": "ACID",
+        "ref": "BLK 6005"
+    },
+    {
+        "hex": "#A9C707",
+        "name": "SLIMER",
+        "ref": "BLK 6010"
+    },
+    {
+        "hex": "#A1A324",
+        "name": "OASIS",
+        "ref": "BLK 6012"
+    },
+    {
+        "hex": "#CBD56C",
+        "name": "SPRING",
+        "ref": "BLK 6013"
+    },
+    {
+        "hex": "#B5CC22",
+        "name": "WILD LIME",
+        "ref": "BLK 6015"
+    },
+    {
+        "hex": "#51AB27",
+        "name": "IRISH GREEN",
+        "ref": "BLK 6045"
+    },
+    {
+        "hex": "#008132",
+        "name": "BOSTON",
+        "ref": "BLK 6055"
+    },
+    {
+        "hex": "#025A27",
+        "name": "CELTIC",
+        "ref": "BLK 6060"
+    },
+    {
+        "hex": "#003613",
+        "name": "BANKNOTE",
+        "ref": "BLK 6065"
+    },
+    {
+        "hex": "#003C1A",
+        "name": "TAG GREEN",
+        "ref": "BLK 6070"
+    },
+    {
+        "hex": "#28C842",
+        "name": "HIPPIE",
+        "ref": "BLK 6075"
+    },
+    {
+        "hex": "#29B419",
+        "name": "MESCALINE",
+        "ref": "BLK 6080"
+    },
+    {
+        "hex": "#25900C",
+        "name": "WOODSTOCK",
+        "ref": "BLK 6090"
+    },
+    {
+        "hex": "#00752D",
+        "name": "PLANT",
+        "ref": "BLK 6095"
+    },
+    {
+        "hex": "#91D1DA",
+        "name": "TIFFANY",
+        "ref": "BLK 6110"
+    },
+    {
+        "hex": "#4AADC4",
+        "name": "COOL COLOGNE",
+        "ref": "BLK 6130"
+    },
+    {
+        "hex": "#0086A1",
+        "name": "MERMAID",
+        "ref": "BLK 6150"
+    },
+    {
+        "hex": "#013C4A",
+        "name": "NEPTUNE",
+        "ref": "BLK 6170"
+    },
+    {
+        "hex": "#002A32",
+        "name": "RIFFS",
+        "ref": "BLK 6175"
+    },
+    {
+        "hex": "#6BDBCF",
+        "name": "DROPS",
+        "ref": "BLK 6180"
+    },
+    {
+        "hex": "#3DC6AC",
+        "name": "NAPPIES",
+        "ref": "BLK 6190"
+    },
+    {
+        "hex": "#0C8E76",
+        "name": "SURGERY",
+        "ref": "BLK 6195"
+    },
+    {
+        "hex": "#9AC776",
+        "name": "E2E GREEN",
+        "ref": "BLK 6210"
+    },
+    {
+        "hex": "#50AE58",
+        "name": "REVOLT GREEN",
+        "ref": "BLK 6220"
+    },
+    {
+        "hex": "#007F64",
+        "name": "ATLANTIS",
+        "ref": "BLK 6240"
+    },
+    {
+        "hex": "#C7DBC8",
+        "name": "OLYMP",
+        "ref": "BLK 6310"
+    },
+    {
+        "hex": "#B0D7BE",
+        "name": "HOPE",
+        "ref": "BLK 6320"
+    },
+    {
+        "hex": "#64B699",
+        "name": "PATINA",
+        "ref": "BLK 6330"
+    },
+    {
+        "hex": "#007764",
+        "name": "COPPER GREEN",
+        "ref": "BLK 6340"
+    },
+    {
+        "hex": "#D1D5BC",
+        "name": "TRABI",
+        "ref": "BLK 6410"
+    },
+    {
+        "hex": "#BEC695",
+        "name": "BEETLE",
+        "ref": "BLK 6420"
+    },
+    {
+        "hex": "#A3AC7C",
+        "name": "RESEDA",
+        "ref": "BLK 6430"
+    },
+    {
+        "hex": "#A5B29C",
+        "name": "FOG",
+        "ref": "BLK 6510"
+    },
+    {
+        "hex": "#8C9C7D",
+        "name": "MIST",
+        "ref": "BLK 6520"
+    },
+    {
+        "hex": "#4C6043",
+        "name": "STORM",
+        "ref": "BLK 6530"
+    },
+    {
+        "hex": "#D1C782",
+        "name": "BAMBOO",
+        "ref": "BLK 6610"
+    },
+    {
+        "hex": "#A3903F",
+        "name": "HEMP",
+        "ref": "BLK 6620"
+    },
+    {
+        "hex": "#8F8534",
+        "name": "BOA",
+        "ref": "BLK 6625"
+    },
+    {
+        "hex": "#705A39",
+        "name": "PAN",
+        "ref": "BLK 6630"
+    },
+    {
+        "hex": "#BAB526",
+        "name": "PEAR GREEN",
+        "ref": "BLK 6710"
+    },
+    {
+        "hex": "#718428",
+        "name": "LOST ISLAND",
+        "ref": "BLK 6720"
+    },
+    {
+        "hex": "#565B21",
+        "name": "TROOPS",
+        "ref": "BLK 6725"
+    },
+    {
+        "hex": "#28401C",
+        "name": "TOAD",
+        "ref": "BLK 6730"
+    },
+    {
+        "hex": "#D6D1AF",
+        "name": "FACE",
+        "ref": "BLK 6905"
+    },
+    {
+        "hex": "#A2A682",
+        "name": "HANNIBAL",
+        "ref": "BLK 6910"
+    },
+    {
+        "hex": "#808460",
+        "name": "MURDOCK",
+        "ref": "BLK 6920"
+    },
+    {
+        "hex": "#3F3E2A",
+        "name": "B.A BOSKO",
+        "ref": "BLK 6930"
+    },
+    {
+        "hex": "#EEF1E6",
+        "name": "JAWS",
+        "ref": "BLK 7010"
+    },
+    {
+        "hex": "#C7C8B8",
+        "name": "MOUSE",
+        "ref": "BLK 7030"
+    },
+    {
+        "hex": "#999A92",
+        "name": "SHARK",
+        "ref": "BLK 7050"
+    },
+    {
+        "hex": "#5E6460",
+        "name": "RHINO",
+        "ref": "BLK 7070"
+    },
+    {
+        "hex": "#48403D",
+        "name": "ANT",
+        "ref": "BLK 7080"
+    },
+    {
+        "hex": "#C7BBA8",
+        "name": "GAMBETTA",
+        "ref": "BLK 7110"
+    },
+    {
+        "hex": "#9D9283",
+        "name": "LENNOX",
+        "ref": "BLK 7120"
+    },
+    {
+        "hex": "#7D7268",
+        "name": "LAMBRATE",
+        "ref": "BLK 7130"
+    },
+    {
+        "hex": "#524038",
+        "name": "INDUSTRIILOR",
+        "ref": "BLK 7140"
+    },
+    {
+        "hex": "#A69FA2",
+        "name": "HOUDINI",
+        "ref": "BLK 7210"
+    },
+    {
+        "hex": "#847C82",
+        "name": "MORPHEUS",
+        "ref": "BLK 7220"
+    },
+    {
+        "hex": "#57515A",
+        "name": "NOSTRADAMUS",
+        "ref": "BLK 7230"
+    },
+    {
+        "hex": "#A89E9F",
+        "name": "GHETTO",
+        "ref": "BLK 7320"
+    },
+    {
+        "hex": "#645B5C",
+        "name": "DUMBO",
+        "ref": "BLK 7340"
+    },
+    {
+        "hex": "#352C2D",
+        "name": "SLATE",
+        "ref": "BLK 7360"
+    },
+    {
+        "hex": "#F2E8C4",
+        "name": "IVORY",
+        "ref": "BLK 8000"
+    },
+    {
+        "hex": "#B89247",
+        "name": "SAND",
+        "ref": "BLK 8010"
+    },
+    {
+        "hex": "#D9B980",
+        "name": "BEIGE",
+        "ref": "BLK 8020"
+    },
+    {
+        "hex": "#DAAC92",
+        "name": "ICED COFFE",
+        "ref": "BLK 8030"
+    },
+    {
+        "hex": "#CB916C",
+        "name": "CREMINO",
+        "ref": "BLK 8040"
+    },
+    {
+        "hex": "#B27A59",
+        "name": "FRAPPÉ",
+        "ref": "BLK 8050"
+    },
+    {
+        "hex": "#7C5035",
+        "name": "CHOCOLATE",
+        "ref": "BLK 8060"
+    },
+    {
+        "hex": "#49201A",
+        "name": "JAWA",
+        "ref": "BLK 8070"
+    },
+    {
+        "hex": "#902C2E",
+        "name": "RUST",
+        "ref": "BLK 8080"
+    },
+    {
+        "hex": "#C2A875",
+        "name": "ARABIAN",
+        "ref": "BLK 8110"
+    },
+    {
+        "hex": "#AB9269",
+        "name": "GOBI",
+        "ref": "BLK 8120"
+    },
+    {
+        "hex": "#90754F",
+        "name": "SYRIAN",
+        "ref": "BLK 8130"
+    },
+    {
+        "hex": "#9F6B5A",
+        "name": "AFTER",
+        "ref": "BLK 8210"
+    },
+    {
+        "hex": "#F5AC8C",
+        "name": "SNAIL",
+        "ref": "BLK 8220"
+    },
+    {
+        "hex": "#DF5105",
+        "name": "KOI",
+        "ref": "BLK 8230"
+    },
+    {
+        "hex": "#B94709",
+        "name": "FALLING",
+        "ref": "BLK 8240"
+    },
+    {
+        "hex": "#5B2607",
+        "name": "CANDY BAR",
+        "ref": "BLK 8250"
+    },
+    {
+        "hex": "#512320",
+        "name": "MUD",
+        "ref": "BLK 8260"
+    },
+    {
+        "hex": "#030506",
+        "name": "BLACK",
+        "ref": "BLK 9001"
+    },
+    {
+        "hex": "#FFFFFF",
+        "name": "SNOW WHITE",
+        "ref": "BLK 9100"
+    },
+    {
+        "hex": "#FBFCF6",
+        "name": "WHITE",
+        "ref": "BLK 9105"
+    },
+    {
+        "hex": "#F9E900",
+        "name": "TRUE YELLOW",
+        "ref": "BLK TR 1000"
+    },
+    {
+        "hex": "#FFF562",
+        "name": "TRUE YEL. 50%",
+        "ref": "BLK TR 1010"
+    },
+    {
+        "hex": "#E51175",
+        "name": "TRUE MAGENTA",
+        "ref": "BLK TR 4000"
+    },
+    {
+        "hex": "#E75D98",
+        "name": "TRUE MAG. 50%",
+        "ref": "BLK TR 4010"
+    },
+    {
+        "hex": "#00B8D2",
+        "name": "TRUE CYAN",
+        "ref": "BLK TR 5000"
+    },
+    {
+        "hex": "#86CEDC",
+        "name": "TRUE CYAN 50%",
+        "ref": "BLK TR 5010"
+    },
+    {
+        "hex": "#FFDF00",
+        "name": "POWER YELLOW",
+        "ref": "P1000"
+    },
+    {
+        "hex": "#FF5A10",
+        "name": "POWER ORANGE",
+        "ref": "P2000"
+    },
+    {
+        "hex": "#E22218",
+        "name": "POWER RED",
+        "ref": "P3000"
+    },
+    {
+        "hex": "#E64690",
+        "name": "POWER PINK",
+        "ref": "P4000"
+    },
+    {
+        "hex": "#18225B",
+        "name": "POWER VIOLET",
+        "ref": "P4100"
+    },
+    {
+        "hex": "#00549D",
+        "name": "POWER BLUE",
+        "ref": "P5000"
+    },
+    {
+        "hex": "#67B028",
+        "name": "POWER GREEN",
+        "ref": "P6000"
+    },
+    {
+        "hex": "#999999",
+        "name": "SILVER SILVERCHROME",
+        "ref": ""
+    },
+    {
+        "hex": "#C0C0C0",
+        "name": "OUTL. SILVER OUTLINE SILVER",
+        "ref": ""
+    },
+    {
+        "hex": "#806031",
+        "name": "COPPER COPPERCHROME",
+        "ref": "0"
+    },
+    {
+        "hex": "#826107",
+        "name": "GOLD GOLDCHROME",
+        "ref": ""
+    }
+
+]
