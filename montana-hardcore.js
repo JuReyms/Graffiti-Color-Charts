58d7594158006acc66599f3a60379a980d8bd2d2
Ju Reyms <65686072+JuReyms@users.noreply.github.com>
2022-04-07 15:49:22 +0200

    Create montana-hardcore.js

---
+++ montana-hardcore.js
@@ -0,0 +1,714 @@
+export default [
+    {
+        "name": "Unicorn Yellow",
+        "hex": "#FFFFBF",
+        "ref": "HRV-252"
+    },
+    {
+        "name": "Beach Yellow",
+        "hex": "#fff386",
+        "ref": "HRV-222"
+    },
+    {
+        "name": "Party Yellow",
+        "hex": "#fdef46",
+        "ref": "HRV-20"
+    },
+    {
+        "name": "Light Yellow",
+        "hex": "#ffe100",
+        "ref": "HRV-1021"
+    },
+    {
+        "name": "Luxor Yellow",
+        "hex": "#FAB700",
+        "ref": "HRV-239"
+    },
+    {
+        "name": "Ganges Yellow",
+        "hex": "#f4ca2a",
+        "ref": "HRV-11"
+    },
+    {
+        "name": "Peach",
+        "hex": "#F9BA57",
+        "ref": "HRV-1017"
+    },
+    {
+        "name": "Medium Yellow",
+        "hex": "#F6A800",
+        "ref": "HRV-1028"
+    },
+    {
+        "name": "Mango",
+        "hex": "#E77A19",
+        "ref": "HRV-207"
+    },
+    {
+        "name": "Mustard",
+        "hex": "#B9501A",
+        "ref": "HRV-8023"
+    },
+    {
+        "name": "Pangea Brown",
+        "hex": "#933B00",
+        "ref": "HRV-261"
+    },
+    {
+        "name": "Rust Red",
+        "hex": "#6D2601",
+        "ref": "HRV-18"
+    },
+    {
+        "name": "Cream",
+        "hex": "#FED792",
+        "ref": "HRV-7"
+    },
+    {
+        "name": "Atacama Yellow",
+        "hex": "#FBCB8C",
+        "ref": "HRV-206"
+    },
+
+    {
+        "name": "Apricot",
+        "hex": "#FAC294",
+        "ref": "HRV-9"
+    },
+    {
+        "name": "Pumpkin",
+        "hex": "#F6AD6B",
+        "ref": "HRV-208"
+    },
+    {
+        "name": "Pastel Orange",
+        "hex": "#EC753B",
+        "ref": "HRV-2003"
+    },
+    {
+        "name": "Orange",
+        "hex": "#E95E0F",
+        "ref": "HRV-2004"
+    },
+    {
+        "name": "Calcutta Orange",
+        "hex": "#DC4126",
+        "ref": "HRV-209"
+    },
+    {
+        "name": "Prometheus Orange",
+        "hex": "#910e14",
+        "ref": "HRV-210"
+    },
+    {
+        "name": "Iroko Red",
+        "hex": "#5E0000",
+        "ref": "HRV-260"
+    },
+    {
+        "name": "Flamingo",
+        "hex": "#FF958E",
+        "ref": "HRV-259"
+    },
+    {
+        "name": "Colorado Red",
+        "hex": "#E74F45",
+        "ref": "HRV-33"
+    },
+    {
+        "name": "Light Red",
+        "hex": "#E5352D",
+        "ref": "HRV-3020"
+    },
+    {
+        "name": "Vivid Red",
+        "hex": "#C90019",
+        "ref": "HRV-3001"
+    },
+    {
+        "name": "Madrid Red",
+        "hex": "#E5352D",
+        "ref": "HRV-241"
+    },
+    {
+        "name": "Soviet Red",
+        "hex": "#8d0000",
+        "ref": "HRV-242"
+    },
+    {
+        "name": "Bordeaux Red",
+        "hex": "#82131D",
+        "ref": "HRV-3004"
+    },
+    {
+        "name": "Alice Pink",
+        "hex": "#F4ADC5",
+        "ref": "HRV-348"
+    },
+    {
+        "name": "Manga Pink",
+        "hex": "#FFA2CA",
+        "ref": "HRV-258"
+    },
+    {
+        "name": "Love Pink",
+        "hex": "#EE87AD",
+        "ref": "HRV-211"
+    },
+    {
+        "name": "Erika",
+        "hex": "#DE336B",
+        "ref": "HRV-4003"
+    },
+    {
+        "name": "Magenta",
+        "hex": "#DD1D5A",
+        "ref": "HRV-4010"
+    },
+    {
+        "name": "Akari Red",
+        "hex": "#C50041",
+        "ref": "HRV-212"
+    },
+    {
+        "name": "Merlot Red",
+        "hex": "#762A3D",
+        "ref": "HRV-213"
+    },
+    {
+        "name": "Treze Violet",
+        "hex": "#F6B8D0",
+        "ref": "HRV-349"
+    },
+    {
+        "name": "Princess Violet",
+        "hex": "#CA87B6",
+        "ref": "HRV-257"
+    },
+    {
+        "name": "Witch Violet",
+        "hex": "#C8739F",
+        "ref": "HRV-224"
+    },
+    {
+        "name": "Geisha Violet",
+        "hex": "#C15C92",
+        "ref": "HRV-225"
+    },
+    {
+        "name": "Pure Purple",
+        "hex": "#8F154B",
+        "ref": "HRV-350"
+    },
+    {
+        "name": "Tube Violet",
+        "hex": "#7C3F60",
+        "ref": "HRV-226"
+    },
+    {
+        "name": "Cherry",
+        "hex": "#582741",
+        "ref": "HRV-4007"
+    },
+    {
+        "name": "Malva",
+        "hex": "#ACA6D1",
+        "ref": "HRV-256"
+    },
+    {
+        "name": "Violet",
+        "hex": "#9993C3",
+        "ref": "HRV-214"
+    },
+    {
+        "name": "Prophet Violet",
+        "hex": "#6D63A5",
+        "ref": "HRV-215"
+    },
+    {
+        "name": "Blue Violet",
+        "hex": "#574A96",
+        "ref": "HRV-3"
+    },
+    {
+        "name": "Anonymous Violet",
+        "hex": "#45347A",
+        "ref": "HRV-216"
+    },
+    {
+        "name": "Galaxy Violet",
+        "hex": "#2F2280",
+        "ref": "HRV-264"
+    },
+    {
+        "name": "Vampire Violet",
+        "hex": "#3E2D65",
+        "ref": "HRV-27"
+    },
+    {
+        "name": "Aurora Blue",
+        "hex": "#C8D3ED",
+        "ref": "HRV-73"
+    },
+    {
+        "name": "Hortensia Blue",
+        "hex": "#889ed1",
+        "ref": "HRV-351"
+    },
+    {
+        "name": "Gala Blue",
+        "hex": "#5d78ba",
+        "ref": "HRV-352"
+    },
+    {
+        "name": "Julione Blue",
+        "hex": "#40549f",
+        "ref": "HRV-353"
+    },
+    {
+        "name": "Babylon Blue",
+        "hex": "#574A96",
+        "ref": "HRV-243"
+    },
+    {
+        "name": "Universe Blue",
+        "hex": "#40549f",
+        "ref": "HRV-354"
+    },
+    {
+        "name": "Lewis Blue",
+        "hex": "#40549f",
+        "ref": "HRV-32"
+    },
+    {
+        "name": "Costa Brava Blue",
+        "hex": "#94BFE5",
+        "ref": "HRV-74"
+    },
+    {
+        "name": "Waimea Blue",
+        "hex": "#6F9FD0",
+        "ref": "HRV-227"
+    },
+    {
+        "name": "Zeppelin Blue",
+        "hex": "#4875B3",
+        "ref": "HRV-228"
+    },
+    {
+        "name": "Andromeda Blue",
+        "hex": "#465499",
+        "ref": "HRV-229"
+    },
+    {
+        "name": "Ultramarine Blue",
+        "hex": "#37306A",
+        "ref": "HRV-5002"
+    },
+    {
+        "name": "Leviathan Blue",
+        "hex": "#0D0F49",
+        "ref": "HRV-230"
+    },
+    {
+        "name": "Nostromo Blue",
+        "hex": "#020E37",
+        "ref": "HRV-75"
+    },
+    {
+        "name": "Himalayan Blue",
+        "hex": "#86CEE1",
+        "ref": "HRV-13"
+    },
+    {
+        "name": "Arctic Blue",
+        "hex": "#6BDCFF",
+        "ref": "HRV-29"
+    },
+    {
+        "name": "Light Blue",
+        "hex": "#60C5E7",
+        "ref": "HRV-8"
+    },
+    {
+        "name": "Avatar Blue",
+        "hex": "#00A2DD",
+        "ref": "HRV-217"
+    },
+    {
+        "name": "Electric Blue",
+        "hex": "#007FC4",
+        "ref": "HRV-30"
+    },
+    {
+        "name": "Dark Blue",
+        "hex": "#00659D",
+        "ref": "HRV-5005"
+    },
+    {
+        "name": "Neptune Blue",
+        "hex": "#004578",
+        "ref": "HRV-218"
+    },
+    {
+        "name": "Fukushima Blue",
+        "hex": "#8ECFD3",
+        "ref": "HRV-355"
+    },
+    {
+        "name": "Cousteau Blue",
+        "hex": "#1BB2C4",
+        "ref": "HRV-231"
+    },
+    {
+        "name": "Patagonia Blue",
+        "hex": "#83CCDF",
+        "ref": "HRV-255"
+    },
+    {
+        "name": "Glacier Blue",
+        "hex": "#009CAB",
+        "ref": "HRV-232"
+    },
+    {
+        "name": "Planet Blue",
+        "hex": "#008098",
+        "ref": "HRV-14"
+    },
+    {
+        "name": "Christiania Blue",
+        "hex": "#006675",
+        "ref": "HRV-233"
+    },
+    {
+        "name": "Indigo Blue",
+        "hex": "#004750",
+        "ref": "HRV-234"
+    },
+    {
+        "name": "Vintage Blue",
+        "hex": "#97D2D0",
+        "ref": "HRV-356"
+    },
+    {
+        "name": "Iceberg Blue",
+        "hex": "#6EC3BF",
+        "ref": "HRV-357"
+    },
+    {
+        "name": "Waterlily Blue",
+        "hex": "#60B3AC",
+        "ref": "HRV-358"
+    },
+    {
+        "name": "Mururoa Blue",
+        "hex": "#008C8D",
+        "ref": "HRV-359"
+    },
+    {
+        "name": "Caribbean Blue",
+        "hex": "#007F80",
+        "ref": "HRV-5021"
+    },
+    {
+        "name": "Major Blue",
+        "hex": "#1F6363",
+        "ref": "HRV-360"
+    },
+    {
+        "name": "Ocean Blue",
+        "hex": "#00454E",
+        "ref": "HRV-5020"
+    },
+    {
+        "name": "Potosi Green",
+        "hex": "#A6D5C6",
+        "ref": "HRV-361"
+    },
+    {
+        "name": "Max Green",
+        "hex": "#9fe5c2",
+        "ref": "HRV-254"
+    },
+    {
+        "name": "Paris Green",
+        "hex": "#8bc8aa",
+        "ref": "HRV-219"
+    },
+    {
+        "name": "UFO Green",
+        "hex": "#5bb89e",
+        "ref": "HRV-220"
+    },
+    {
+        "name": "Surgical Green",
+        "hex": "#009473",
+        "ref": "HRV-21"
+    },
+    {
+        "name": "Dark Green",
+        "hex": "#008352",
+        "ref": "HRV-6016"
+    },
+    {
+        "name": "Persephone Green",
+        "hex": "#005231",
+        "ref": "HRV-221"
+    },
+    {
+        "name": "Golden Green",
+        "hex": "#9CCD9A",
+        "ref": "HRV-24"
+    },
+    {
+        "name": "Light Green",
+        "hex": "#74B957",
+        "ref": "HRV-4"
+    },
+    {
+        "name": "Guacamole Green",
+        "hex": "#88b14f",
+        "ref": "HRV-34"
+    },
+    {
+        "name": "Valley Green",
+        "hex": "#6A9720",
+        "ref": "HRV-6018"
+    },
+    {
+        "name": "Cologno N. Green",
+        "hex": "#6a8515",
+        "ref": "HRV-237"
+    },
+    {
+        "name": "Lutecia Green",
+        "hex": "#005900",
+        "ref": "HRV-5"
+    },
+    {
+        "name": "Amazonas Green",
+        "hex": "#0F2405",
+        "ref": "HRV-6009"
+    },
+    {
+        "name": "Poison Green",
+        "hex": "#dedd3a",
+        "ref": "HRV-235"
+    },
+    {
+        "name": "Mojito Green",
+        "hex": "#ced50a",
+        "ref": "HRV-236"
+    },
+    {
+        "name": "Pistachio Green",
+        "hex": "#b0c643",
+        "ref": "HRV-16"
+    },
+    {
+        "name": "Mantis Green",
+        "hex": "#78BE7A",
+        "ref": "HRV-362"
+    },
+    {
+        "name": "Mint Green",
+        "hex": "#60B565",
+        "ref": "HRV-272"
+    },
+    {
+        "name": "Natura Green",
+        "hex": "#00964F",
+        "ref": "HRV-363"
+    },
+    {
+        "name": "Reggae Green",
+        "hex": "#006942",
+        "ref": "HRV-364"
+    },
+    {
+        "name": "Pale Green",
+        "hex": "#CCE0AC",
+        "ref": "HRV-6019"
+    },
+    {
+        "name": "Element Green",
+        "hex": "#c5cc8f",
+        "ref": "HRV-253"
+    },
+    {
+        "name": "Apple Green",
+        "hex": "#acb681",
+        "ref": "HRV-15"
+    },
+    {
+        "name": "Rambo Green",
+        "hex": "#8f995b",
+        "ref": "HRV-250"
+    },
+    {
+        "name": "Khaki Green",
+        "hex": "#727e48",
+        "ref": "HRV-6013"
+    },
+    {
+        "name": "Olive Green",
+        "hex": "#60683d",
+        "ref": "HRV-6003"
+    },
+    {
+        "name": "Forest Green",
+        "hex": "#46502c",
+        "ref": "HRV-251"
+    },
+    {
+        "name": "Druid Brown",
+        "hex": "#eecf99",
+        "ref": "HRV-246"
+    },
+    {
+        "name": "Tepuy Brown",
+        "hex": "#deb276",
+        "ref": "HRV-247"
+    },
+    {
+        "name": "Baobab Brown",
+        "hex": "#cc9456",
+        "ref": "HRV-248"
+    },
+    {
+        "name": "Greyhound Brown",
+        "hex": "#b0763f",
+        "ref": "HRV-249"
+    },
+    {
+        "name": "Toasted Brown",
+        "hex": "#8a5531",
+        "ref": "HRV-8002"
+    },
+    {
+        "name": "Chocolate Brown",
+        "hex": "#4d2200",
+        "ref": "HRV-35"
+    },
+    {
+        "name": "Tobacco Brown",
+        "hex": "#402009",
+        "ref": "HRV-8017"
+    },
+    {
+        "name": "Puchineli White",
+        "hex": "#F9F7EA",
+        "ref": "HRV-365"
+    },
+    {
+        "name": "Bone White",
+        "hex": "#F3EFD6",
+        "ref": "HRV-1013"
+    },
+    {
+        "name": "Arkalis Grey",
+        "hex": "#EBE6D6",
+        "ref": "HRV-55"
+    },
+    {
+        "name": "City Grey",
+        "hex": "#CABDB0",
+        "ref": "HRV-57"
+    },
+    {
+        "name": "Regina Grey",
+        "hex": "#B7A794",
+        "ref": "HRV-42"
+    },
+    {
+        "name": "Nazgul Grey",
+        "hex": "#928782",
+        "ref": "HRV-40"
+    },
+    {
+        "name": "Ripley Grey",
+        "hex": "#7E7F82",
+        "ref": "HRV-39"
+    },
+    {
+        "name": "Light Grey",
+        "hex": "#878787",
+        "ref": "HRV-6"
+    },
+    {
+        "name": "Squatter Grey",
+        "hex": "#C4C5C6",
+        "ref": "HRV-366"
+    },
+    {
+        "name": "Matter Grey",
+        "hex": "#7b7b7b",
+        "ref": "HRV-262"
+    },
+    {
+        "name": "Pearl Grey",
+        "hex": "#7B7C7E",
+        "ref": "HRV-7040"
+    },
+    {
+        "name": "Dark Grey",
+        "hex": "#616264",
+        "ref": "HRV-7031"
+    },
+    {
+        "name": "Sputnik Grey",
+        "hex": "#575757",
+        "ref": "HRV-263"
+    },
+    {
+        "name": "Anthracite Grey",
+        "hex": "#464648",
+        "ref": "HRV-7016"
+    },
+    {
+        "name": "Black",
+        "hex": "#000000",
+        "ref": "HRV-9011"
+    },
+    {
+        "name": "Matte Black",
+        "hex": "#000000",
+        "ref": "HRV-Matte-Black"
+    },
+    {
+        "name": "Satin Black",
+        "hex": "#000000",
+        "ref": "HRV-Satin-Black"
+    },
+    {
+        "name": "White",
+        "hex": "#FFFFFF",
+        "ref": "HRV-9010"
+    },
+    {
+        "name": "Satin White",
+        "hex": "#FFFFFF",
+        "ref": "HRV-Satin-White"
+    },
+    {
+        "name": "Matte White",
+        "hex": "#FFFFFF",
+        "ref": "HRV-Matte-White"
+    },
+
+    {
+        "name": "Gold",
+        "hex": "#D3BF8C",
+        "ref": "HRV-Gold"
+    },
+    {
+        "name": "Silver",
+        "hex": "#C0C0C0",
+        "ref": "HRV-silver"
+    },
+    {
+        "name": "Copper",
+        "hex": "#93746b",
+        "ref": "HRV-copper"
+    }
+]
