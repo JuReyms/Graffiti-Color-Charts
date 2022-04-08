e0ebf40c3c2a88a4c0db2d28b49ccfd8d8bdbd81
Ju Reyms <65686072+JuReyms@users.noreply.github.com>
2022-04-08 13:23:16 +0200

    Create montana-water-based.js

---
+++ montana-water-based.js
@@ -0,0 +1,492 @@
+export default [
+    {
+      "name": "Cadmium Yellow Light",
+      "ref": "RV-222",
+      "hex": "#FFE885"
+    },
+    {
+      "name": "Cadmium Yellow Medium",
+      "ref": "RV-1021",
+      "hex": "#FFDD35"
+    },
+    {
+      "name": "Azo Yellow Deep",
+      "ref": "RV-177",
+      "hex": "#EAB63E"
+    },
+    {
+      "name": "Yellow Ocre",
+      "ref": "RV-264",
+      "hex": "#BA8032"
+    },
+    {
+      "name": "Raw Sienna",
+      "ref": "RV-265",
+      "hex": "#9D6F3C"
+    },
+    {
+      "name": "Azo Orange Pale",
+      "ref": "RV-102",
+      "hex": "#F7B792"
+    },
+    {
+      "name": "Azo Orange Light",
+      "ref": "RV-105",
+      "hex": "#FF9955"
+    },
+    {
+      "name": "Azo Orange",
+      "ref": "RV-2004",
+      "hex": "#FF6F22"
+    },
+    {
+      "name": "Azo Orange Deep",
+      "ref": "RV-209",
+      "hex": "#F05136"
+    },
+    {
+      "name": "Azo Orange Dark",
+      "ref": "RV-347",
+      "hex": "#D30C14"
+    },
+    {
+      "name": "Cadmium Red Pale",
+      "ref": "RV-322",
+      "hex": "#F39C9A"
+    },
+    {
+      "name": "Cadmium Red Light",
+      "ref": "RV-223",
+      "hex": "#EA746A"
+    },
+    {
+      "name": "Naphthol Red",
+      "ref": "RV-3020",
+      "hex": "#DE4343"
+    },
+    {
+      "name": "Naphthol Red Deep",
+      "ref": "RV-241",
+      "hex": "#B11000"
+    },
+    {
+      "name": "Carmine",
+      "ref": "RV-3004",
+      "hex": "#8E0029"
+    },
+    {
+      "name": "Quinacridone Rose Light",
+      "ref": "RV-323",
+      "hex": "#EE82A4"
+    },
+    {
+      "name": "Quinacridone Rose",
+      "ref": "RV-211",
+      "hex": "#EE82A4"
+    },
+    {
+      "name": "Quinacridone Magenta",
+      "ref": "RV-4010",
+      "hex": "#D84465"
+    },
+    {
+      "name": "Red Violet",
+      "ref": "RV-213",
+      "hex": "#B60052"
+    },
+    {
+      "name": "Red Violet Deep",
+      "ref": "RV-324",
+      "hex": "#7C3253"
+    },
+    {
+      "name": "Blue Violet Pale",
+      "ref": "RV-325",
+      "hex": "#DEA6BD"
+    },
+    {
+      "name": "Blue Violet Light",
+      "ref": "RV-224",
+      "hex": "#CC8FA8"
+    },
+    {
+      "name": "Blue Violet",
+      "ref": "RV-225",
+      "hex": "#B74D78"
+    },
+    {
+      "name": "Blue Violet Deep",
+      "ref": "RV-167",
+      "hex": "#87436D"
+    },
+    {
+      "name": "Blue Violet Dark",
+      "ref": "RV-169",
+      "hex": "#5B364D"
+    },
+    {
+      "name": "Dioxazine Purple Pale",
+      "ref": "RV-321",
+      "hex": "#DACBEE"
+    },
+    {
+      "name": "Dioxazine Purple Light",
+      "ref": "RV-214",
+      "hex": "#C199C8"
+    },
+    {
+      "name": "Dioxazine Purple",
+      "ref": "RV-173",
+      "hex": "#6F71B9"
+    },
+    {
+      "name": "Dioxazine Purple Deep",
+      "ref": "RV-174",
+      "hex": "#765EAD"
+    },
+    {
+      "name": "Dioxazine Purple Dark",
+      "ref": "RV-28",
+      "hex": "#280773"
+    },
+    {
+      "name": "Phthalo Blue Pale",
+      "ref": "RV-326",
+      "hex": "#CCF3F0"
+    },
+    {
+      "name": "Phthalo Blue Light",
+      "ref": "RV-29",
+      "hex": "99DBE5"
+    },
+    {
+      "name": "Cerulean Blue",
+      "ref": "RV-217",
+      "hex": "#009ECC"
+    },
+    {
+      "name": "Prussian Blue",
+      "ref": "RV-30",
+      "hex": "#1366A0"
+    },
+    {
+      "name": "Prussian Blue Deep",
+      "ref": "RV-154",
+      "hex": "#005082"
+    },
+    {
+      "name": "Cobalt Blue Pale",
+      "ref": "RV-327",
+      "hex": "#88C5E4"
+    },
+    {
+      "name": "Cobalt Blue Light",
+      "ref": "RV-69",
+      "hex": "#4492C6"
+    },
+    {
+      "name": "Cobalt Blue",
+      "ref": "RV-68",
+      "hex": "#28628E"
+    },
+    {
+      "name": "Ultramarine Blue",
+      "ref": "RV-5002",
+      "hex": "#385988"
+    },
+    {
+      "name": "Ultramarine Blue Deep",
+      "ref": "RV-328",
+      "hex": "#2E3A62"
+    },
+    {
+      "name": "Primary Blue Pale",
+      "ref": "RV-336",
+      "hex": "#5CA5DA"
+    },
+    {
+      "name": "Primary Blue Light",
+      "ref": "RV-337",
+      "hex": "#5388C6"
+    },
+    {
+      "name": "Primary Blue",
+      "ref": "RV-338",
+      "hex": "#245BA7"
+    },
+    {
+      "name": "Primary Blue Deep",
+      "ref": "RV-339",
+      "hex": "#23388D"
+    },
+    {
+      "name": "Primary Blue Dark",
+      "ref": "RV-340",
+      "hex": "#25348A"
+    },
+    {
+      "name": "Blue Green Pale",
+      "ref": "RV-341",
+      "hex": "#88CDD3"
+    },
+    {
+      "name": "Blue Green Light",
+      "ref": "RV-342",
+      "hex": "#06B5D2"
+    },
+    {
+      "name": "Blue Green",
+      "ref": "RV-343",
+      "hex": "#028DB4"
+    },
+    {
+      "name": "Blue Green Deep",
+      "ref": "RV-14",
+      "hex": "#00789A"
+    },
+    {
+      "name": "Blue Green Dark",
+      "ref": "RV-234",
+      "hex": "#005476"
+    },
+    {
+      "name": "Phathalo Green Blue",
+      "ref": "RV-254",
+      "hex": "#BBE9DE"
+    },
+    {
+      "name": "Turquoise Green",
+      "ref": "RV-219",
+      "hex": "#00D2B3"
+    },
+    {
+      "name": "Emerald Green Light",
+      "ref": "RV-220",
+      "hex": "#57B798"
+    },
+    {
+      "name": "Emerald Green",
+      "ref": "RV-21",
+      "hex": "#00957E"
+    },
+    {
+      "name": "Emeral Green Deep",
+      "ref": "RV-221",
+      "hex": "#055F5D"
+    },
+    {
+      "name": "Brillant Yellow Green Light",
+      "ref": "RV-235",
+      "hex": "#E6E782"
+    },
+    {
+      "name": "Brillant Yellow Green",
+      "ref": "RV-236",
+      "hex": "#CED664"
+    },
+    {
+      "name": "Brillant Yellow Green Medium",
+      "ref": "RV-333",
+      "hex": "#9BBE53"
+    },
+    {
+      "name": "Brillant Yellow Green Deep",
+      "ref": "RV-334",
+      "hex": "#569426"
+    },
+    {
+      "name": "Brillant Yellow Green Dark",
+      "ref": "RV-335",
+      "hex": "#36792B"
+    },
+    {
+      "name": "Phathalo Green Light",
+      "ref": "RV-124",
+      "hex": "#C8DD9B"
+    },
+    {
+      "name": "Phathalo Green",
+      "ref": "RV-329",
+      "hex": "#9CCC95"
+    },
+    {
+      "name": "Brillant Light Green",
+      "ref": "RV-34",
+      "hex": "#67B346"
+    },
+    {
+      "name": "Brillant Green",
+      "ref": "RV-6018",
+      "hex": "#56944F"
+    },
+    {
+      "name": "Brillant Green Deep",
+      "ref": "RV-127",
+      "hex": "#255C1E"
+    },
+    {
+      "name": "Grey Green Pale",
+      "ref": "RV-344",
+      "hex": "#C4CBA9"
+    },
+    {
+      "name": "Grey Green Light",
+      "ref": "RV-345",
+      "hex": "#BEC39E"
+    },
+    {
+      "name": "Grey Green",
+      "ref": "RV-179",
+      "hex": "#757A4F"
+    },
+    {
+      "name": "Grey Green Deep",
+      "ref": "RV-131",
+      "hex": "#647253"
+    },
+    {
+      "name": "Grey Green Dark",
+      "ref": "RV-346",
+      "hex": "#4A5D45"
+    },
+    {
+      "name": "Titanium light",
+      "ref": "RV-1013",
+      "hex": "#F3DA9A"
+    },
+    {
+      "name": "Naples Yellow",
+      "ref": "RV-135",
+      "hex": "#F4CF81"
+    },
+    {
+      "name": "Naples Yellow Deep",
+      "ref": "RV-330",
+      "hex": "#D7B483"
+    },
+    {
+      "name": "Raw Umber",
+      "ref": "RV-137",
+      "hex": "#B0946F"
+    },
+    {
+      "name": "Raw Umber Deep",
+      "ref": "RV-139",
+      "hex": "#745B48"
+    },
+    {
+      "name": "Warm Grey Pale",
+      "ref": "RV-331",
+      "hex": "#D9D6CF"
+    },
+    {
+      "name": "Warm Grey Light",
+      "ref": "RV-302",
+      "hex": "#CbC2B0"
+    },
+    {
+      "name": "Warm Grey Medium",
+      "ref": "RV-303",
+      "hex": "#C0B6A4"
+    },
+    {
+      "name": "Warm Grey Deep",
+      "ref": "RV-332",
+      "hex": "#645C59"
+    },
+    {
+      "name": "Burnt Umber",
+      "ref": "RV-266",
+      "hex": "#66564F"
+    },
+    {
+      "name": "Neutral Grey Pale",
+      "ref": "RV-6",
+      "hex": "#D2D7D8"
+    },
+    {
+      "name": "Neutral Grey Light",
+      "ref": "RV-7047",
+      "hex": "#B4B8B6"
+    },
+    {
+      "name": "Neutral Grey",
+      "ref": "RV-7040",
+      "hex": "#A5AAA8"
+    },
+    {
+      "name": "Neutral Grey Deep",
+      "ref": "RV-263",
+      "hex": "#676C73"
+    },
+    {
+      "name": "Neutral Grey Dark",
+      "ref": "RV-7016",
+      "hex": "#555357"
+    },
+    {
+      "name": "Fluorescent Yellow",
+      "ref": "RV-Fluo-1",
+      "hex": "#FFED39"
+    },
+    {
+      "name": "Fluorescent Red",
+      "ref": "RV-Fluo-2",
+      "hex": "#F95951"
+    },
+    {
+      "name": "Fluorescent Magenta",
+      "ref": "RV-Fluo-2",
+      "hex": "#FF0093"
+    },
+    {
+      "name": "Fluorescent Green",
+      "ref": "RV-Fluo-3",
+      "hex": "#60DD49"
+    },
+    {
+      "name": "Fluorescent Blue",
+      "ref": "RV-Fluo-4",
+      "hex": "#00AACC"
+    },
+    {
+      "name": "Carbon Black",
+      "ref": "RV-Carbon",
+      "hex": "#000000"
+    },
+    {
+      "name": "Titanium White",
+      "ref": "RV-Titanium",
+      "hex": "#FFFFFF"
+    },
+    {
+      "name": "Silver",
+      "ref": "RV-Silver",
+      "hex": "#999999"
+    },
+    {
+      "name": "Gold",
+      "ref": "RV-Gold",
+      "hex": "#826107"
+    },
+    {
+      "name": "Transparent Black",
+      "ref": "RV-T-Black",
+      "hex": "#000000"
+    },
+    {
+      "name": "Transparent White",
+      "ref": "RV-T-White",
+      "hex": "#FFFFFF"
+    },
+    {
+      "name": "Glossy Varnish",
+      "ref": "RV-Glossy",
+      "hex": "#f1f1f1"
+    },
+    {
+      "name": "Matt Varnish",
+      "ref": "RV-Matt",
+      "hex": "#f3f3f3"
+    }
+  ]
